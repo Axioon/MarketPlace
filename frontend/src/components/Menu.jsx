eeb707527be14a64303ec0ae909244c457055715
Ricardo Moena <122154547+Axioon@users.noreply.github.com>
2024-04-21 03:22:57 -0400

    install axios npm

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -3,7 +3,7 @@ const Menu = () => {
         <>
             <div className="container mx-auto py-8 bg-cover bg-center" style={{ backgroundImage: "url('https://www.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2023/05/torre-gaming-3036108.jpg')" }}>
                 <div className="bg-black bg-opacity-50 py-8">
-                    <h1 className="text-3xl font-semibold text-center text-white mb-6">Axxion Technology</h1>
+                    <h1 className="text-3xl font-semibold text-center text-white mb-6">Axion Technology</h1>
                     <p className="text-lg text-gray-300 text-center mb-8">Encuentra los mejores productos para mejorar tu experiencia de juego</p>
                 </div>
             </div>
