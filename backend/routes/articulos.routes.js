10584eb7edd257c75ad5eacbd3f1d7d02ed51906
1Leeee <127454037+1Leeee@users.noreply.github.com>
2024-04-28 18:46:53 -0400

    Ultimos cambios y arreglos de login

--- backend/routes/articulos.routes.js
+++ backend/routes/articulos.routes.js
@@ -1,14 +1,19 @@
 //articulos.routes.js
 
-import { Router } from "express";
+import express from 'express';
 import { getArticulo,createPost,deletePost,putPost } from "../src/controllers/articulos.controller.js";
 import { obtenerArticulosConOferta } from '../src/controllers/articulo.oferta.controller.js';
+import { registerUser, loginUser } from '../src/controllers/auth.controller.js';
 
-const router = Router();
+const router = express.Router();
+
 router.get('/articulo',getArticulo)
 router.post('/articulo',createPost)
 router.put('/articulo/:id',putPost)
 router.delete('/articulo/:id',deletePost)
+
+router.post('/auth/register', registerUser);
+router.post('/auth/login', loginUser);
 
 
 router.get('/articulos-con-oferta', obtenerArticulosConOferta);
