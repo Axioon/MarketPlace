462ebb6bdb64f5e3dd5e274be1a50d4affa3fa04
FelipeLampard <felipe.araya.fs@gmail.com>
2024-05-06 18:45:17 -0400

    imagenes y arreglos info db

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -21,24 +21,27 @@ function Categories({ category }) {
     }, [category.id]);
 
     const ArticleGrid = ({ articles }) => (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {articles.map(producto => (
-                <div key={producto.id} className="bg-white rounded-lg shadow-md">
-                    <img src={producto.img} alt={producto.nombre} className="w-full h-64 object-cover object-center rounded-t-lg" />
-                    <div className="p-4">
-                        <h2 className="text-xl font-semibold mb-2">{producto.nombre}</h2>
-                        <p className="text-gray-700 mb-2">Descripción: {producto.descripcion}</p>
-                        <p className="text-gray-700 mb-2">Precio: ${producto.precio}</p>
-                        <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">Agregar al carrito</button>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {articles.map(producto => (
+            <div key={producto.id} className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <img src={producto.img} alt={producto.nombre} className="w-full h-80 object-cover object-center" />
+                <div className="p-6">
+                    <h2 className="text-xl font-semibold mb-4">{producto.nombre}</h2>
+                    <p className="text-gray-700 mb-4">Descripción: {producto.descripcion}</p>
+                    <p className="text-gray-700 mb-4">Precio: ${producto.precio}</p>
+                    <div className="flex justify-center"> 
+                        <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-md transition duration-300 ease-in-out">Agregar al carrito</button>
                     </div>
                 </div>
-            ))}
-        </div>
+            </div>
+        ))}
+    </div>
+    
     );
 
     return (
         <div className="container mx-auto mt-8">
-            <h1 className="text-3xl font-bold mb-4">Categoría: {category.name}</h1>
+            <h1 className="text-3xl font-bold mb-8">{category.name}</h1>
             <ArticleGrid articles={articles} />
         </div>
     );
