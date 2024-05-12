173a8c4b0945678d8ec6e0550e422775156d09c2
Ricardo Moena <122154547+Axioon@users.noreply.github.com>
2024-05-12 15:10:32 -0400

    building carrito two

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -7,6 +7,13 @@ export function Modal() {
   const [modalAbierto, setModalAbierto] = useState(false);
   const { carrito, agregarAlCarrito, eliminarDelCarrito, eliminarTodoElCarrito } = useCarrito();
 
+  // Función para abrir el modal cuando se agrega un artículo al carrito
+  useEffect(() => {
+    if (carrito.length > 0) {
+      setModalAbierto(true);
+    }
+  }, [carrito]);
+
   const toggleModal = () => {
     setModalAbierto(!modalAbierto);
   };
@@ -23,13 +30,6 @@ export function Modal() {
     eliminarTodoElCarrito();
   };
 
-  useEffect(() => {
-    // Cerrar el modal cuando se eliminen todos los elementos del carrito
-    if (carrito.length === 0) {
-      setModalAbierto(false);
-    }
-  }, [carrito]);
-
   // Calcular el precio total de todos los artículos en el carrito
   const precioTotal = carrito.reduce((total, producto) => {
     return total + (producto.precio * producto.cantidad);
@@ -43,31 +43,29 @@ export function Modal() {
 
       <input id="modalCheckbox" type="checkbox" hidden checked={modalAbierto} onChange={toggleModal} />
 
-      {modalAbierto && (
-        <aside className="Modal">
-          <div className="Modal-content">
-            <ul className="Modal-list">
-              {carrito.map(producto => (
-                <li key={producto.id}>
-                  <img src={producto.img} alt={producto.nombre} />
-                  <footer> 
-                    <small className="text-white">{producto.nombre}</small>
-                    <small className="text-white">Precio: ${Number((producto.precio * producto.cantidad).toFixed(2))}</small>
-                    <small className="text-white">Cantidad: {producto.cantidad}</small>
-                    <button onClick={() => handleAgregarProducto(producto)} className=" bg-green-400">+</button>
-                    <button onClick={() => handleQuitarProducto(producto.id)} className=" bg-red-400">-</button>
-                  </footer>
-                </li>
-              ))}
-            </ul>
-            <div className="Modal-footer">
-              <p className="text-white   border-l-neutral-200">Precio Total: ${precioTotal.toFixed(2)}</p>
-              <button onClick={handleEliminarCarrito} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">Limpiar</button>
-              <button className="bg-green-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md ml-2">Comprar</button>
-            </div>
+      <aside className={`Modal ${modalAbierto ? 'open' : ''}`}>
+        <div className="Modal-content">
+          <ul className="Modal-list">
+            {carrito.map(producto => (
+              <li key={producto.id}>
+                <img src={producto.img} alt={producto.nombre} />
+                <footer> 
+                  <small className="text-white">{producto.nombre}</small>
+                  <small className="text-white">Precio: ${Number((producto.precio * producto.cantidad).toFixed(2))}</small>
+                  <small className="text-white">Cantidad: {producto.cantidad}</small>
+                  <button onClick={() => handleAgregarProducto(producto)} className=" bg-green-400">+</button>
+                  <button onClick={() => handleQuitarProducto(producto.id)} className=" bg-red-400">-</button>
+                </footer>
+              </li>
+            ))}
+          </ul>
+          <div className="Modal-footer">
+            <p className="text-white   border-l-neutral-200">Precio Total: ${precioTotal.toFixed(2)}</p>
+            <button onClick={handleEliminarCarrito} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">Limpiar</button>
+            <button className="bg-green-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md ml-2">Comprar</button>
           </div>
-        </aside>
-      )}
+        </div>
+      </aside>
     </>
   );
 }
